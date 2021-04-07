f74077a975463796e45172e06695efeb0d27ad17
joshua jackson <jjackson17mv@gmail.com>
2021-04-06 19:50:46 -0700

    added css, fixed functional components, add footer and header.js

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+                <Navbar dark color="">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Ristorante con Fusion</h1>
+                                <p>We take inspiration from the work we do, and the worlds best cuisines internationally</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </Jumbotron>
+            </>
+        );
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap';
 import './index.css';
 import App from './App';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
