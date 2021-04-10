5d979ed5161dbd7bbbd6486fc1c6c86dc5dd8588
joshua jackson <jjackson17mv@gmail.com>
2021-04-10 13:20:50 -0700

    added react router and links, navbar components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
-import { DISHES } from '../shared/dishes';
-import Header from './Header';
-import Footer from './Footer';
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import DishDetail from "./DishDetailComponent";
+import { DISHES } from "../shared/dishes";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
+
 
 //function App() 
 class Main extends Component {
@@ -14,25 +17,30 @@ class Main extends Component {
     this.state = {
       // this dishes object will be passed child component "Menu"
       dishes: DISHES,
-      selectedDish: null
-    }
+      // selectedDish: null
+    };
 
   }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
+  //onDishSelect(dishId) {
+  // this.setState({ selectedDish: dishId });
+  //}
 
 
   render() {
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
     return (
       <div>
         <Header />
-
-        <Menu dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail
-          dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      <Footer/>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+          </Switch>
+        <Footer />
       </div>
     );
   }
